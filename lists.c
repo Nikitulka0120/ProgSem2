cca3c7dc0ce83b22affe7ba5acdd679e0b14648c
Nikitulka0120 <107490259+Nikitulka0120@users.noreply.github.com>
2025-03-03 21:19:54 +0700

    Update lists.c

--- lists.c
+++ lists.c
@@ -31,9 +31,10 @@ void printList(node_t *head)
 
 void printElement(node_t *element)
 {
-    node_t *cur = element;
-    printf("%d ", cur->data);
-    printf("\n");
+    if (element)
+        printf("%d\n", element->data);
+    else
+        printf("Элемент не найден!\n");
 }
 
 void addLast(node_t **head, int data)
@@ -59,64 +60,89 @@ void addSecond(node_t **head, int data, node_t **S)
     if (*S == NULL) 
     {
         *S = new;
+        if (*head != NULL)
+        {
+            (*head)->second_next = *S;
+        }
         return;
     }
-
-    node_t *cur_first = *head;
+    
     node_t *cur_second = *S;
-
     while (cur_second->next != NULL)
     {   
         cur_second = cur_second->next;
-        cur_first = cur_first->second_next;
     }
     cur_second->next = new;
+    
+    node_t *cur_first = *head;
+    while (cur_first !=NULL && cur_first->second_next)
+    {
+        cur_first = cur_first->next;
+    }
+    cur_first->second_next = new;
 }
-
 
 int main()
 {
-    // // node_t *node1 = newNode(1);
-    // // node_t *node2 = newNode(2);
-    // // node_t *node3 = newNode(3);
-    // node1->next = node2;
-    // node2->next = node3;
-
     node_t *head = NULL;
     int n;
+    
+    printf("Введите числа для первого списка (0 для завершения):\n");
     while (1)
     {
-        printf("\nВведите число:");
         scanf("%d", &n);
-        if (n==0){
+        if (n == 0)
             break;
-        }
         addLast(&head, n);
     }
 
     node_t *S = NULL;
-    printf("\nПервый список\n");
+    printf("\nПервый список:\n");
     printList(head);
+
+    printf("\nВведите числа для второго списка (0 для завершения):\n");
     while (1)
     {
-        printf("\nВведите число второго списка:");
         scanf("%d", &n);
-        if (n==0){
+        if (n == 0)
             break;
-        }
         addSecond(&head, n, &S);
     }
-    printList(S);
-    char con;
-    printf("Управление: \nW-Печать текущего элемента списка\nD-Печать следующего элемента списка\nS-Печать нижнего элемента\nA-Печать начального элемента текущего списка");
-    scanf("%c", &con);
-    node_t *current=head;
-    if (con == "W"){
-        printElement(&current);
-    }
-        elif (con == "W"){
-        printElement(&current);
-    }
 
+    printf("\nВторой список:\n");
+    printList(S);
+
+    node_t *current = head;
+    char con;
+    
+    printf("\nУправление: \nW - Печать текущего элемента списка\nD - Печать следующего элемента списка\nS - Печать нижнего элемента\nA - Печать начального элемента текущего списка\n");
+    
+    while (1)
+    {
+        scanf(" %c", &con);
+        switch (con)
+        {
+            case 'W':
+                printElement(current);
+                break;
+            case 'D':
+                if (current && current->next)
+                    current = current->next;
+                printElement(current);
+                break;
+            case 'S':
+                if (current && current->second_next)
+                    current = current->second_next;
+                printElement(current);
+                break;
+            case 'A':
+                current = head;
+                printElement(current);
+                break;
+            default:
+                printf("Неизвестная команда!\n");
+                break;
+        }
+    }
     return 0;
 }
